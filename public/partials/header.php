764d296419ab17c522328734b36ef18244fe3145
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-10-21 01:02:14 +0800

    feat: add favicon

--- public/partials/header.php
+++ public/partials/header.php
@@ -5,6 +5,11 @@
   <meta charset="UTF-8">
   <title><?= $pageTitle ?? 'C is for Cookie' ?></title>
   <link rel="stylesheet" href="/assets/index.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+  <link rel="shortcut icon" href='/assets/favicon/favicon.ico' />
+  <link rel="manifest" href="/assets/site.webmanifest">
 </head>
 
 <body>
