4e3fa134674238c60e7864040c43a7f0f8f86721
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-10-20 13:26:28 +0800

    feat: scaffold about and privacy policy pages

---
+++ public/about.php
@@ -0,0 +1,7 @@
+<?php
+$pageTitle = "About";
+include 'partials/header.php';
+?>
+<h2>About Page</h2>
+<p>This is the about page</p>
+<?php include 'partials/footer.php'; ?>
---
+++ public/privacy-policy.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = "Privacy Policy";
+include 'partials/header.php';
+?>
+<h2>Privacy Policy Page</h2>
+<p>Ullamco occaecat aute reprehenderit pariatur velit reprehenderit eu laborum duis pariatur occaecat qui nostrud.
+  Consectetur nisi sit eu excepteur anim nostrud fugiat qui duis proident irure laboris. Velit dolor fugiat mollit
+  reprehenderit sunt non culpa elit commodo sint non. Commodo voluptate eiusmod non dolor. Pariatur exercitation quis
+  aliqua minim culpa tempor id Lorem. Incididunt enim non exercitation deserunt commodo amet deserunt aliquip. Mollit
+  laborum dolore proident. Non sint ullamco nostrud sit ea aliqua id ex.</p>
+
+<p>Elit nisi sit sint labore consequat sint excepteur nisi ea. Lorem eu aliqua ex officia amet et eu sunt ad cupidatat
+  tempor. Do laboris in amet veniam Lorem quis magna labore et esse sunt. Do est veniam velit magna consectetur
+  laborum ut est fugiat. Nulla in ut tempor consequat est nisi ex officia ad id ullamco dolore. Enim tempor ex aliquip
+  id quis nostrud minim. Excepteur nostrud nisi elit labore dolore. Et cillum culpa est.</p>
+
+<?php include 'partials/footer.php'; ?>
