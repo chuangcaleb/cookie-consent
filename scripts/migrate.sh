ac48cf06efd5b4eb97cee6d3afcb1b87391d0936
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-10-21 00:07:24 +0800

    fix: migrate `ssl ` property

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -23,7 +23,7 @@ for f in $(ls migrations/*.sql | sort); do
   echo "🔹 Applying $f ..."
 
   mysql \
-    --ssl-mode=TRUE \
+    --ssl=DISABLED \
     --ssl-verify-server-cert=OFF \
     -h "$DB_HOST" \
     -P "${DB_PORT:-3306}" \
