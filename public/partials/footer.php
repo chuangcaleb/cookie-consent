0fd41061cd6e92ec846623cb0f86ed8af4e64c1c
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-10-20 13:40:13 +0800

    feat: add terms/privacy to footer

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,6 +1,9 @@
 </main>
 <footer>
-  <p>&copy; <?= date('Y') ?> My PHP Site</p>
+  <span>
+    <a href="/terms.php">Terms & Conditions</a> ⋅ <a href="privacy-policy.php">Privacy Statement</a> ⋅
+  </span>
+  &copy; <?= date('Y') ?> My PHP Site</p>
 </footer>
 </body>
 
