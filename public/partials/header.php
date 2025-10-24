412c9d7fd0dcca804388c5b6c3738655a1df30f7
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2025-10-24 18:34:36 +0800

    feat: add site title to title in head

--- public/partials/header.php
+++ public/partials/header.php
@@ -15,7 +15,7 @@ $excluded = ['terms.php', 'privacy.php'];
 
 <head>
   <meta charset="UTF-8">
-  <title><?= $pageTitle ?? 'C is for Cookie' ?></title>
+  <title><?= ($pageTitle ?? false) ? "$pageTitle | C is for Cookies" : 'C is for Cookie' ?></title>
   <link rel="stylesheet" href="/assets/index.css">
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
